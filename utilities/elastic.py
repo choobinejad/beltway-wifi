fe46be582a82b8be3da0d71a41509e250cd9a6c7
jeremywoodworth <jeremy.m.woodworth@gmail.com>
2019-03-05 22:15:04 -0500

    #4 turn off cert verification to accommodate self-signed test env

--- utilities/elastic.py
+++ utilities/elastic.py
@@ -4,6 +4,7 @@ from elasticsearch import Elasticsearch
 def get_elastic_client(hosts, user, password):
     es = Elasticsearch(
         hosts=hosts,
-        http_auth=(user, password)
+        http_auth=(user, password),
+        verify_certs=False
     )
     return es
